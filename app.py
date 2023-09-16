3dfbd75e792815f22801f2442073319d55295c9c
James Hathaway <112409964+james-hathaway@users.noreply.github.com>
2023-09-16 12:42:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@
 from pytesseract import image_to_string
 import pytesseract
 import nltk
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import argparse
 
 # Download necessary nltk datasets if they aren't already
@@ -14,20 +13,40 @@ if not nltk.data.find("corpora/stopwords"):
 if not nltk.data.find("tokenizers/punkt"):
     nltk.download("punkt")
 
-myconfig = r"--psm 6 --oem 3"
+def preprocess_image(img_path):
+    """Pre-process the image for better OCR output."""
+    img = Image.open(img_path)
+    
+    # Resize the image
+    img = img.resize((img.width*2, img.height*2), Image.LANCZOS)
+    
+    # Convert image to grayscale
+    img = img.convert('L')
+    
+    # Enhance the contrast of the image
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(2)
+    
+    # Apply a sharpening filter
+    img = img.filter(ImageFilter.SHARPEN)
+    
+    return img
 
 # Use argparse to get the image filename from the command line argument
 parser = argparse.ArgumentParser(description='Process an image and extract text using pytesseract.')
 parser.add_argument('image_name', type=str, help='The exact name/path of your image file.')
 args = parser.parse_args()
 
-image = Image.open(args.image_name)
+image = preprocess_image(args.image_name)
+
+# Fine-tuned config for better OCR output
+myconfig = r"--psm 11 --oem 3"
+
 output_text = pytesseract.image_to_string(image, config=myconfig)
 
 # Pre-process the OCR output
-stop_words = set(stopwords.words("english"))
 tokens = word_tokenize(output_text)
-tokens = [t for t in tokens if t.lower() not in stop_words and t.isalnum()]  # Added t.isalnum() to remove punctuation
+tokens = [t for t in tokens if t.isalnum()]  # Only keep alphanumeric tokens
 
 # Save the pre-processed text to files as per user's choice
 while True:
